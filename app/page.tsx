f2c6bba7605b21db70b5bf04acffa18496ef14f6
fizalangah <fizalangah2001@gmail.com>
2024-09-26 04:42:38 -0700

    responsiv

--- app/page.tsx
+++ app/page.tsx
@@ -26,26 +26,26 @@ export default function Tipcalculator() {
       };
   return (
     <div className="flex justify-center items-center text-left">
-        <div className="h-[500px] w-[400px] bg-white border-4 rounded-xl border-white  mt-[100px] shadow-xl ">
+        <div className="h-[500px] lg:w-[400px] w-[300px] bg-white border-4 rounded-xl border-white  mt-[100px] shadow-xl ">
            <h1 className="mt-[30px] ml-[30px] font-bold ">Tip-Calculator</h1> 
            <p className="ml-[30px] text-sm mt[10px] font-serif">Enter the bill amount and tip percentage to calculate the tip and total.</p>
-           <div className="ml-[30px] mt-[20px]  flex flex-col w-[350px] font-semibold">
+           <div className="ml-[30px] mt-[20px]  flex flex-col lg:w-[350px] md:w-[250px] w-[150px] font-semibold">
            <label>Bill Amount</label>
            <input type="number"
            onChange={handleBillAmountChange}
             placeholder="Enter bill amount " className="rounded-xl p-2 outline-none" />
            </div>
-           <div className="ml-[30px] mt-[20px]  flex flex-col w-[350px] font-semibold">
+           <div className="ml-[30px] mt-[20px]  flex flex-col lg:-[350px] md:w-[250px]  w-[150px]font-semibold">
            <label>Tip Percentage</label>
            <input type="number"
            onChange={handleTipPercentageChange}
             placeholder="Enter Tip percentage " className="rounded-xl p-2 outline-none" />
            </div>
              <button onClick={calculateTip} className="bg-black p-3 rounded-xl ml-[30px] text-white mt-[30px]">calculate</button>
-             <div className="ml-[30px] mt-[30px] flex flex-row gap-x-40 font-semibold">
+             <div className="ml-[30px] mt-[30px] flex flex-row lg:gap-x-40 gap-x-[50px] font-semibold">
              <h3>Tip Amount</h3> <p>{` $${tipAmount.toFixed(2)}`}</p>
              </div>
-             <div className="ml-[20px] mt-[30px] flex flex-row gap-x-40 font-semibold">
+             <div className="ml-[20px] mt-[30px] flex flex-row lg:gap-x-40  gap-x-[50px] font-semibold">
              <h3>Total Amount:</h3> <p>{` $${totalAmount.toFixed(2)}`}</p>
              </div>
         </div> 
